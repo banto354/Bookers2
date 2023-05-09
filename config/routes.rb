20f8e727f6f1ffdb993240b4090665eb20ea1c34
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-05-09 08:32:44 +0000

    [Add] bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/edit'
+  get 'users/show'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to:"homes#top"
+  root to: "homes#top"
 end
