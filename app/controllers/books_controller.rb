ace2dc49486ebfa4babd7ace723711f01504d56c
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-05-10 08:08:15 +0000

    [Save] editing index.html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,14 @@
 class BooksController < ApplicationController
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 end
