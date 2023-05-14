acb094cbdc3015d9a6021aa6469ebf1693f9a62c
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-05-14 10:03:25 +0000

    [Finish] App_Task_3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,7 +1,9 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_id: book.id )
+    book_comment = BookComment.new(book_comment_params)
+    book_comment.user_id = current_user.id 
+    book_comment.book_id = book.id
     book_comment.save
     redirect_to book_path(book.id)
   end
@@ -11,5 +13,11 @@ class BookCommentsController < ApplicationController
     book = book_comment.book
     book_comment.destroy
     redirect_to book_path(book.id)
+  end
+  
+  private
+  
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
   end
 end
