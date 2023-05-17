4d14eb9b9e070b4507cd0c2cf5fd6ea37db2bbef
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-05-17 01:42:42 +0000

    [Add] routes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,23 +1,21 @@
 class SearchesController < ApplicationController
-  def show
-  end
-  
+
   def search
     target = params[:target]
     type = params[:type]
-    query = params[:query]
+    @query = params[:query]
     
     if target == "User"
       if type == "whole"
-        @users = User.where("name LIKE ?", "#{query}")
+        @users = User.where("name LIKE ?", "#{@query}")
       elsif type == "part"
-        @users = User.where("name LIKE ?", "%#{query}%")
+        @users = User.where("name LIKE ?", "%#{@query}%")
       end
     elsif target == "Book"
       if type == "whole"
-        @books = Book.where("title LIKE ?", "#{query}")
+        @books = Book.where("title LIKE ?", "#{@query}")
       elsif target == "part"
-        @books = Book.where("title LIKE ?", "%#{query}%")
+        @books = Book.where("title LIKE ?", "%#{@query}%")
       end
     else 
       redirect_to request.referer
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'searches/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
   get 'relationships/:user_id/followers' => 'relationships#followers', as: 'followers'
   get 'relationships/:user_id/following' => 'relationships#following', as: 'following'
+  get 'searches/search' => 'searches#search', as: 'search'
   
   resources :books, only: [:new, :index, :show, :create, :destroy, :edit, :update] do
     resource :favorites, only: [:create, :destroy]
