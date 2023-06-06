44ec6c890ecafe39c1e477206a6275a841e1d5ad
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-06-06 13:57:38 +0000

    Edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,20 +1,43 @@
 class GroupsController < ApplicationController
   def new
-    @gourp = Group.new
+    @group = Group.new
   end
   
   def show
   end
 
   def index
+    @user = User.find(current_user.id)
+    @n_book = Book.new
+    @groups = Group.all
   end
 
   def create
-    @group = Group.new
+    @group = Group.new(group_params)
     @group.owner_id = current_user.id
+    @group.save
+    @user = User.find(current_user.id)
     redirect_to groups_path
   end
 
   def edit
+  end
+  
+  def destroy
+    group = Group.find(params[:id])
+    group.destroy
+    redirect_to '/groups'
+  end
+  private
+  
+  def group_params
+    params.require(:group).permit(:name,:introduction)
+  end
+  
+  def is_matching_login_user
+    group = Group.find(params[:id])
+    unless group.owner_id == current_user.id
+      redirect_to groups_path 
+    end
   end
 end
