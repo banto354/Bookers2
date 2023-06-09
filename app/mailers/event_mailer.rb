d8b9334967d252bc8154dda4edc6096d38e363b8
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-06-09 08:35:23 +0000

    Finish 9c

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -3,6 +3,7 @@ class EventMailer < ApplicationMailer
     def event_mail(event, group_users)
         @event = event
         group_users.each do |group_user|
+            @group_user = group_user
             mail to: group_user.user.email, bcc: ENV["GMAIL_USER"], subject: "#{@event.title}"
         end
     end
